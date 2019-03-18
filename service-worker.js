97e3ec440d84270aeb88d5906b13e2e0d7bf4d05
Tommy Li <thomasli2020@u.northwestern.edu>
2019-03-17 23:41:25 -1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32803d1a9b874da0b27bc69d856b0b4e.js"
+  "/precache-manifest.e54f469e7c6251ab986ff4a00310af69.js"
 );
 
 workbox.clientsClaim();
